0ec5198a2b3e84af53ca0103adeda1c154d6f56d
crackcodesprogramloads <59869248+crackcodesprogramloads@users.noreply.github.com>
2023-11-12 16:17:40 +0700

    Add check for past targetBlockNumber, add votingPower function

--- contracts/TokenizedBallot.sol
+++ contracts/TokenizedBallot.sol
@@ -17,19 +17,31 @@ contract TokenizedBallot {
 
     constructor(
         bytes32[] memory _proposalNames,
-        address _tokenContract,
+        IMyToken _tokenContract,
         uint256 _targetBlockNumber
     ) {
         tokenContract = IMyToken(_tokenContract);
         targetBlockNumber = _targetBlockNumber;
-        // TODO: Validate if targetBlockNumber is in the past
+        require(
+            _targetBlockNumber < block.number,
+            "TokenizedBallot: Target block number must be in the past"
+        );
         for (uint i = 0; i < _proposalNames.length; i++) {
             proposals.push(Proposal({name: _proposalNames[i], voteCount: 0}));
         }
     }
 
     function vote(uint256 proposal, uint256 amount) external {
-        // TODO: Implement vote function
+        require(
+            votingPower(msg.sender) >= amount,
+            "TokenizedBallot: trying to vote more than allowed"
+        );
+        proposals[proposal].voteCount += amount;
+    }
+
+    function votingPower(address account) public view returns (uint256) {
+        return tokenContract.getPastVotes(account, targetBlockNumber);
+        // TODO: check if this is enough for protecting the contract
     }
 
     function winningProposal() public view returns (uint winningProposal_) {
