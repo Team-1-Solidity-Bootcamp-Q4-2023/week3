415a9ef8bba2ef3632461374fc435e8b5428d73d
Tomo <tomo@crowdeo.com>
2023-11-13 20:51:25 +0700

    fin

---
+++ README.md
@@ -0,0 +1,92 @@
+
+output from 0m0t running: ts-node ./scripts/DeployWithEthers.ts 
+
+
+```
+Account 1 address: 0xE3c382A8B72643CC3756D532e967Eb44e885c619
+
+Token contract deployed at 0x004138fEFc2966B995c32b6104b6f8aF00948065
+
+Minted 1000000000000000000 decimal units to account 0xE3c382A8B72643CC3756D532e967Eb44e885c619
+[https://sepolia.etherscan.io/tx/0x4a092988d5fae857d3e9da3d153c83af3fbf4a7b54802cd4b1aba18af47dc29d]
+
+Account 0xE3c382A8B72643CC3756D532e967Eb44e885c619 has 1000000000000000000 decimal units of MyToken
+
+Account 0xE3c382A8B72643CC3756D532e967Eb44e885c619 has 0 units of voting power before self delegating
+
+Account 0xE3c382A8B72643CC3756D532e967Eb44e885c619 has 1000000000000000000 units of voting power after self delegating
+[https://sepolia.etherscan.io/tx/0xa6c11119bbe08edfa17ffb49a6f6ac126ab02ea4b543ba4cc11b19265358172a]
+
+Account 0xE3c382A8B72643CC3756D532e967Eb44e885c619 has 500000000000000000 units of voting power after transferring
+[https://sepolia.etherscan.io/tx/0x006e3187a749fd3451609ddf0082bb8e2bdb3e97b0286440d21be19242665602]
+
+Account 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 has 0 units of voting power after receiving a transfer
+
+TokenizedBallot contract deployed at 0x77A5480FC861a00686B9ceA73a37115BCEc75B0D
+
+acc1's votingPower: 0.5
+acc2's votingPower: 0.0
+vote tx hash: 0x419ef24f35f36fc363e04f44adc8e1f34be0d8bdab71147a33471d1770735b4c
+[https://sepolia.etherscan.io/tx/0x419ef24f35f36fc363e04f44adc8e1f34be0d8bdab71147a33471d1770735b4c]
+
+acc1's votingPower: 0.25
+caught Error: execution reverted: "TokenizedBallot: trying to vote more than allowed" [...continued below...]
+
+vote remainder tx hash: 0xe0c7d90f9063d5c41b28408c3e87283c4b63936e058c168b6a225a34065082be
+[https://sepolia.etherscan.io/tx/0xe0c7d90f9063d5c41b28408c3e87283c4b63936e058c168b6a225a34065082be]
+
+acc1's votingPower: 0.0
+vote 0 tx hash: 0xed5dfefa1306037592c20f9b4aeebc22c5ef6af4f34914d2c6705d24bb8a9715
+[https://sepolia.etherscan.io/tx/0xed5dfefa1306037592c20f9b4aeebc22c5ef6af4f34914d2c6705d24bb8a9715]
+
+winning proposal: Proposal 2
+
+```
+
+
+
+
+
+
+## caught
+
+```
+caught Error: execution reverted: "TokenizedBallot: trying to vote more than allowed" (action="estimateGas", data="0x08c379a000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000031546f6b656e697a656442616c6c6f743a20747279696e6720746f20766f7465206d6f7265207468616e20616c6c6f776564000000000000000000000000000000", reason="TokenizedBallot: trying to vote more than allowed", transaction={ "data": "0xb384abef000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000006f05b59d3b20000", "from": "0xE3c382A8B72643CC3756D532e967Eb44e885c619", "to": "0x77A5480FC861a00686B9ceA73a37115BCEc75B0D" }, invocation=null, revert={ "args": [ "TokenizedBallot: trying to vote more than allowed" ], "name": "Error", "signature": "Error(string)" }, code=CALL_EXCEPTION, version=6.8.1)
+    at makeError (/home/tomo/dev/week3/node_modules/ethers/src.ts/utils/errors.ts:694:21)
+    at getBuiltinCallException (/home/tomo/dev/week3/node_modules/ethers/src.ts/abi/abi-coder.ts:118:21)
+    at Function.getBuiltinCallException (/home/tomo/dev/week3/node_modules/ethers/src.ts/abi/abi-coder.ts:230:16)
+    at JsonRpcProvider.getRpcError (/home/tomo/dev/week3/node_modules/ethers/src.ts/providers/provider-jsonrpc.ts:906:32)
+    at /home/tomo/dev/week3/node_modules/ethers/src.ts/providers/provider-jsonrpc.ts:526:45
+    at processTicksAndRejections (node:internal/process/task_queues:95:5) {
+  code: 'CALL_EXCEPTION',
+  action: 'estimateGas',
+  data: '0x08c379a000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000031546f6b656e697a656442616c6c6f743a20747279696e6720746f20766f7465206d6f7265207468616e20616c6c6f776564000000000000000000000000000000',
+  reason: 'TokenizedBallot: trying to vote more than allowed',
+  transaction: {
+    to: '0x77A5480FC861a00686B9ceA73a37115BCEc75B0D',
+    data: '0xb384abef000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000006f05b59d3b20000',
+    from: '0xE3c382A8B72643CC3756D532e967Eb44e885c619'
+  },
+  invocation: null,
+  revert: {
+    signature: 'Error(string)',
+    name: 'Error',
+    args: [ 'TokenizedBallot: trying to vote more than allowed' ]
+  },
+  shortMessage: 'execution reverted: "TokenizedBallot: trying to vote more than allowed"',
+  info: {
+    error: {
+      code: 3,
+      data: '0x08c379a000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000031546f6b656e697a656442616c6c6f743a20747279696e6720746f20766f7465206d6f7265207468616e20616c6c6f776564000000000000000000000000000000',
+      message: 'execution reverted: TokenizedBallot: trying to vote more than allowed'
+    },
+    payload: {
+      method: 'eth_estimateGas',
+      params: [Array],
+      id: 151,
+      jsonrpc: '2.0'
+    }
+  }
+}
+
+```
--- scripts/DeployWithEthers.ts
+++ scripts/DeployWithEthers.ts
@@ -1,16 +1,130 @@
 import { ethers } from "ethers";
-import { MyToken__factory } from "../typechain-types";
+import { MyToken__factory, TokenizedBallot__factory } from "../typechain-types";
 import 'dotenv/config';
 require('dotenv').config();
 
+const MINT_VALUE = ethers.parseEther("1");
+
 async function main() {
+    // const [deployer, acc1, acc2] = await ethers.getSigners();
     const provider = new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT_URL ?? "");
+
     const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", provider)
+    const acc1 = { address: await wallet.getAddress() }; // deployer
+    const acc2 = { address: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266' }; // hardhat[0]
+    console.log(`Account 1 address: ${acc1.address}\n`);
     const contractFactory = new MyToken__factory(wallet);
     const contract = await contractFactory.deploy();
     await contract.waitForDeployment();
     const contractAddress = await contract.getAddress();
     console.log(`Token contract deployed at ${contractAddress}\n`);
+
+    const mintTx = await contract.mint(acc1.address, MINT_VALUE);
+    // const mintTx = await contract.mint(acc1.address, MINT_VALUE / 2n);
+    await mintTx.wait();
+    console.log(
+        `Minted ${MINT_VALUE.toString()} decimal units to account ${acc1.address}\n`
+    );
+    const balanceBN = await contract.balanceOf(acc1.address);
+    console.log(
+        `Account ${acc1.address
+        } has ${balanceBN.toString()} decimal units of MyToken\n`
+    );
+
+    const votes = await contract.getVotes(acc1.address);
+    console.log(
+        `Account ${acc1.address
+        } has ${votes.toString()} units of voting power before self delegating\n`
+    );
+
+    const delegateTx = await contract.delegate(acc1.address);
+    await delegateTx.wait(3);
+    const votesAfter = await contract.getVotes(acc1.address);
+    console.log(
+        `Account ${acc1.address
+        } has ${votesAfter.toString()} units of voting power after self delegating\n`
+    );
+
+    // Warning! Error encountered during contract execution[out of gas] 
+    //   receipt: TransactionReceipt {
+    //     provider: JsonRpcProvider {},
+    //     to: '0x0e13fDFcdb7B157ccaC656Bb0ccb3fa405Ff6004',
+    //     from: '0xE3c382A8B72643CC3756D532e967Eb44e885c619',
+    //     contractAddress: null,
+    //     hash: '0x8dc8cb89b4e6759b4b154ce163898171fc7b1b3d75f42f74ad8322fd46e5e4af',
+    //     index: 153,
+    //     blockHash: '0x3ce2ce2f01b4f7c93d07dd4bc28321a228eb0300d41d7f24b49df402c60145c0',
+    //     blockNumber: 4685322,
+    //     logsBloom: '0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
+    //     gasUsed: 67561n,
+    //     cumulativeGasUsed: 29975147n,
+    //     gasPrice: 1000015495n,
+    //     type: 2,
+    //     status: 0,
+    //     root: undefined
+    // },
+    // shortMessage: 'transaction execution reverted'
+
+    const transferTx = await contract.transfer(acc2.address, MINT_VALUE / 2n);
+    await transferTx.wait(2);
+    const votes1AfterTransfer = await contract.getVotes(acc1.address);
+    console.log(
+        `Account ${acc1.address
+        } has ${votes1AfterTransfer.toString()} units of voting power after transferring\n`
+    );
+    const votes2AfterTransfer = await contract.getVotes(acc2.address);
+    console.log(
+        `Account ${acc2.address
+        } has ${votes2AfterTransfer.toString()} units of voting power after receiving a transfer\n`
+    );
+
+    const lastBlock = await provider.getBlock("latest");
+    const lastBlockNumber = lastBlock?.number ?? 0;
+    if (lastBlockNumber === 0) {
+        throw new Error("lastBlockNumber is 0");
+    }
+
+    // const contractAddress = '0x4dadC2cbb3e3373Eed1C5C3c0c53f9d01e362EfC';
+    const deployer = wallet;
+
+    const PROPOSALS = ["Proposal 1", "Proposal 2", "Proposal 3"];
+    const tokenizedBallotFactory = new TokenizedBallot__factory(deployer);
+    const TokenizedBallot = await tokenizedBallotFactory.deploy(
+        PROPOSALS.map(ethers.encodeBytes32String),
+        contractAddress,
+        lastBlockNumber - 1
+    );
+    await TokenizedBallot.waitForDeployment();
+    const contractTokenizedBallotAddress = await TokenizedBallot.getAddress();
+    console.log(`TokenizedBallot contract deployed at ${contractTokenizedBallotAddress}\n`);
+
+    console.log(`acc1's votingPower:`, ethers.formatEther(await TokenizedBallot.votingPower(acc1.address)));
+    console.log(`acc2's votingPower:`, ethers.formatEther(await TokenizedBallot.votingPower(acc2.address)));
+
+    let tx;
+    tx = await TokenizedBallot.vote(1, MINT_VALUE / 4n);
+    await tx.wait();
+    console.log('vote tx hash:', (await tx.wait())?.hash);
+    console.log(`acc1's votingPower:`, ethers.formatEther(await TokenizedBallot.votingPower(acc1.address)));
+    try {
+        tx = await TokenizedBallot.vote(2, MINT_VALUE / 2n);
+        await tx.wait();
+        console.log(tx)
+    } catch (e) {
+        console.log('caught', e);
+        // return;
+    }
+    tx = await TokenizedBallot.vote(1, MINT_VALUE / 4n);
+    await tx.wait();
+    console.log('vote remainder tx hash:', (await tx.wait())?.hash);
+    tx = await TokenizedBallot.vote(1, 0n);
+    await tx.wait();
+    console.log(`acc1's votingPower:`, ethers.formatEther(await TokenizedBallot.votingPower(acc1.address)));
+    console.log('vote 0 tx hash:', (await tx.wait())?.hash);
+    console.log('winning proposal:', ethers.decodeBytes32String(await TokenizedBallot.winnerName()));
+
+
+
 }
 
 main().catch((error) => {
